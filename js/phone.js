8a682ee384dd9a6a8d11152aba6adbc022bcc5b7
kazimuntasirrahman <kazimuntasirrahman@gmail.com>
2024-03-01 09:29:05 +0600

    first commit

---
+++ js/phone.js
@@ -0,0 +1,34 @@
+const url = `https://openapi.programming-hero.com/api/phones?search=a`;
+
+const loadPhone = async () =>{
+  const res = await fetch(url);
+  const data = await res.json();
+  const phones = data.data;
+  displayPhone(phones);
+}
+
+const phoneContainer = document.getElementById('phone-container');
+let i = 0;
+
+const displayPhone = (phones) =>{
+  for(const phone of phones){
+    i++;
+    phoneContainer.innerHTML += `<div class="card w-96 bg-base-100 shadow-xl">
+    <figure><img src="${phone.image}" alt="phone" />
+    </figure>
+    <div class="card-body">
+      <h2 class="card-title">${phone.phone_name}</h2>
+      <p>If a dog chews shoes whose shoes does he choose? If a dog chews shoes whose shoes does he choose?</p>
+      <h2 class="card-title">${phone.slug}</h2>
+      <div class="card-actions justify-end">
+        <button class="btn btn-primary">Show Details</button>
+      </div>
+    </div>
+  </div>`
+}
+  document.getElementById('searchResult').innerHTML = i;
+}
+
+
+
+loadPhone();
